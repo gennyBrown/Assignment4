2e74eec44d8bf881b7f03419aa537d9f3ed2b8b1
tcumings <taysulli@gmail.com>
2019-11-15 16:55:49 -0800

    added .h for Customer and CustomerDatabase classes

---
+++ Assignment4/Customers.h
@@ -0,0 +1,35 @@
+#ifndef ASSIGNMENT4_CUSTOMERS_H
+#define ASSIGNMENT4_CUSTOMERS_H
+
+using namespace std;
+
+class Transaction;
+class Movie;
+
+class Customer
+{
+public:
+    int CustomerId;
+    string FirstName;
+    string LastName;
+    void showHistory();
+    void updateHistory();
+private:
+    vector<Transaction*> History;
+    vector<Movie*> CurrentlyBorrowed;
+};
+
+class CustomerDatabase
+{
+public:
+    bool add(Customer C);
+    bool remove(int ID);
+    Customer* getCustomer(int ID);
+    bool clear();
+private:
+    vector<Customer> Customers;
+    int getHash(int ID);
+};
+
+
+#endif //ASSIGNMENT4_CUSTOMERS_H
